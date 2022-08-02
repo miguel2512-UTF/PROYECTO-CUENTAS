746a013b3a40638ca2181bfdc821309fdfdd2589
miguel2512-UTF <miguelangelwilchez1001@gmail.com>
2022-08-02 10:17:04 -0500

    Creacion del proyecto

---
+++ src/main/java/com/cuentas/proyecto_cuentas/ProyectoCuentasApplication.java
@@ -0,0 +1,13 @@
+package com.cuentas.proyecto_cuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCuentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCuentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuentas/proyecto_cuentas/ProyectoCuentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuentas.proyecto_cuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCuentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
