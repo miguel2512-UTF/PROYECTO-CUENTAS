f7eabf08451bbdc4f4f4256e55dbaf70f1490dea
miguel2512-UTF <miguelangelwilchez1001@gmail.com>
2022-11-05 16:06:12 -0500

    Arreglo de estilos logout y configuración de rutas

--- src/main/java/com/cuentas/proyectocuentas/security/UnauthenticatedRequestHandler.java
+++ src/main/java/com/cuentas/proyectocuentas/security/UnauthenticatedRequestHandler.java
@@ -13,17 +13,17 @@ class UnauthenticatedRequestHandler implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        if (request.getServletPath().startsWith("/usuario/")) {
+        if (request.getServletPath().startsWith("/usuario")) {
             response.sendRedirect("/proyecto/login");
-        } else if (request.getServletPath().startsWith("/compromiso/")) {
+        } else if (request.getServletPath().startsWith("/compromiso")) {
             response.sendRedirect("/proyecto/login");
-        } else if (request.getServletPath().startsWith("/tipocompromiso/")) {
+        } else if (request.getServletPath().startsWith("/tipocompromiso")) {
             response.sendRedirect("/proyecto/login");
-        } else if (request.getServletPath().startsWith("/prestamo/")) {
+        } else if (request.getServletPath().startsWith("/prestamo")) {
             response.sendRedirect("/proyecto/login");
-        } else if (request.getServletPath().startsWith("/prestamoabono/")) {
+        } else if (request.getServletPath().startsWith("/prestamoabono")) {
             response.sendRedirect("/proyecto/login");
-        } else if (request.getServletPath().startsWith("/pagoprestamo/")) {
+        } else if (request.getServletPath().startsWith("/pagoprestamo")) {
             response.sendRedirect("/proyecto/login");
         } else if (request.getServletPath().startsWith("/inicio")) {
             response.sendRedirect("/proyecto/login");
